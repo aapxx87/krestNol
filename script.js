f68ed8a6494cecc255b8b47c8d5e9f6c376ed286
aapxx87 <57785764+aapxx87@users.noreply.github.com>
2021-09-01 21:35:56 +0300

    update

--- script.js
+++ script.js
@@ -21,6 +21,15 @@ const caseWin1 = [[], []]
 const caseWin2 = [[], []]
 const caseWin3 = [[], []]
 
+// вертикаль
+const caseWin4 = [[], []]
+const caseWin5 = [[], []]
+const caseWin6 = [[], []]
+
+// косая
+const caseWin7 = [[], []]
+const caseWin8 = [[], []]
+
 
 boxes.forEach(function (box) {
   box.addEventListener('click', function () {
@@ -31,18 +40,70 @@ boxes.forEach(function (box) {
 
       const classBox = box.classList[1]
 
-      if (classBox === 'box-1' || classBox === 'box-2' || classBox === 'box-3') {
+      if (classBox === 'box-1') {
+
         caseWin1[player].push('x')
-      } else if (classBox === 'box-4' || classBox === 'box-5' || classBox === 'box-6') {
+        caseWin4[player].push('x')
+        caseWin7[player].push('x')
+
+      } else if (classBox === 'box-2') {
+
+        caseWin1[player].push('x')
+        caseWin5[player].push('x')
+
+      } else if (classBox === 'box-3') {
+
+        caseWin1[player].push('x')
+        caseWin6[player].push('x')
+        caseWin8[player].push('x')
+
+      } else if (classBox === 'box-4') {
+
         caseWin2[player].push('x')
-      } else if (classBox === 'box-7' || classBox === 'box-8' || classBox === 'box-9') {
+        caseWin4[player].push('x')
+
+      } else if (classBox === 'box-5') {
+
         caseWin2[player].push('x')
+        caseWin5[player].push('x')
+        caseWin7[player].push('x')
+        caseWin8[player].push('x')
+
+      } else if (classBox === 'box-6') {
+
+        caseWin2[player].push('x')
+        caseWin6[player].push('x')
+
+      } else if (classBox === 'box-7') {
+
+        caseWin3[player].push('x')
+        caseWin4[player].push('x')
+        caseWin8[player].push('x')
+
+      } else if (classBox === 'box-8') {
+
+        caseWin3[player].push('x')
+        caseWin5[player].push('x')
+
+      } else if (classBox === 'box-9') {
+
+        caseWin3[player].push('x')
+        caseWin6[player].push('x')
+        caseWin7[player].push('x')
+
       }
 
 
       console.log(caseWin1);
 
-      if (caseWin1[player].length === 3 || caseWin2[player].length === 3 || caseWin3[player].length === 3) {
+      if (caseWin1[player].length === 3 ||
+        caseWin2[player].length === 3 ||
+        caseWin3[player].length === 3 ||
+        caseWin4[player].length === 3 ||
+        caseWin5[player].length === 3 ||
+        caseWin6[player].length === 3 ||
+        caseWin7[player].length === 3 ||
+        caseWin8[player].length === 3) {
         console.log('Winner Player X ');
         winnerTitle.textContent = 'Winner Player X'
       }
@@ -55,21 +116,73 @@ boxes.forEach(function (box) {
 
       const classBox = box.classList[1]
 
-      if (classBox === 'box-1' || classBox === 'box-2' || classBox === 'box-3') {
-        caseWin1[player].push('o')
-      } else if (classBox === 'box-4' || classBox === 'box-5' || classBox === 'box-6') {
-        caseWin2[player].push('0')
-      } else if (classBox === 'box-7' || classBox === 'box-8' || classBox === 'box-9') {
-        caseWin2[player].push('')
+      if (classBox === 'box-1') {
+
+        caseWin1[player].push('x')
+        caseWin4[player].push('x')
+        caseWin7[player].push('x')
+
+      } else if (classBox === 'box-2') {
+
+        caseWin1[player].push('x')
+        caseWin5[player].push('x')
+
+      } else if (classBox === 'box-3') {
+
+        caseWin1[player].push('x')
+        caseWin6[player].push('x')
+        caseWin8[player].push('x')
+
+      } else if (classBox === 'box-4') {
+
+        caseWin2[player].push('x')
+        caseWin4[player].push('x')
+
+      } else if (classBox === 'box-5') {
+
+        caseWin2[player].push('x')
+        caseWin5[player].push('x')
+        caseWin7[player].push('x')
+        caseWin8[player].push('x')
+
+      } else if (classBox === 'box-6') {
+
+        caseWin2[player].push('x')
+        caseWin6[player].push('x')
+
+      } else if (classBox === 'box-7') {
+
+        caseWin3[player].push('x')
+        caseWin4[player].push('x')
+        caseWin8[player].push('x')
+
+      } else if (classBox === 'box-8') {
+
+        caseWin3[player].push('x')
+        caseWin5[player].push('x')
+
+      } else if (classBox === 'box-9') {
+
+        caseWin3[player].push('x')
+        caseWin6[player].push('x')
+        caseWin7[player].push('x')
+
       }
 
 
       console.log(caseWin1);
 
-      if (caseWin1[player].length === 3 || caseWin2[player].length === 3 || caseWin3[player].length === 3) {
-
+      if (
+        caseWin1[player].length === 3 ||
+        caseWin2[player].length === 3 ||
+        caseWin3[player].length === 3 ||
+        caseWin4[player].length === 3 ||
+        caseWin5[player].length === 3 ||
+        caseWin6[player].length === 3 ||
+        caseWin7[player].length === 3 ||
+        caseWin8[player].length === 3) {
+        console.log('Winner Player X ');
         winnerTitle.textContent = 'Winner Player O'
-        console.log('Winner Player O ');
       }
 
       player = 0
